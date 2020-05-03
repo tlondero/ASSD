fb3ee42f576f8315eeb19cbc79884c2504ec1026
Guido <glambertucci@itba.edu.ar>
2020-05-03 19:20:13 -0300

    dos notas + delay + endtime

--- TP2/EjemploGuidoPortaudio/Otra version de karporongus mas limpia/KARPORONGUS/KARPORONGUS/main.cpp
+++ TP2/EjemploGuidoPortaudio/Otra version de karporongus mas limpia/KARPORONGUS/KARPORONGUS/main.cpp
@@ -10,14 +10,13 @@
 #include <iostream>
 #include "Guitar.h"
 #include "Drum.h"
-using namespace std;
 
+#
 #define SAMPLE_RATE     44100
 #define MAX_AMP         32760
 #define TWO_PI          6.283185307179586476925286766559
 
 using namespace std;
-
 namespace little_endian_io
 {
 	template <typename Word>
@@ -35,16 +34,22 @@ int main(void) {
 	int fs = 44100;
 	double rf = 1;
 	double prob =  0.5;
-	int seconds = 10;
+	int wavDuration = 5;
+	int durationNote = 2;
+	double delay = 0;
+	double cut = 0.3;
 	double volume=800;
 	Guitar myGuitar(freq[1], fs,rf);
-	vector<double> GuitarSound;
+	Guitar myGuitar2(freq[3], fs, rf);
 	Drum myDrum(freq[3],fs,rf,prob);
 	vector<double> DrumSound;
-	for (int i = 0; i < seconds*fs/2; i++) {
-		GuitarSound.push_back(myGuitar.getSample());
+	vector<double> GuitarSound1 =myGuitar.composeSound(cut, durationNote,delay, wavDuration);
+	vector<double> GuitarSound2 = myGuitar2.composeSound(cut, durationNote, 1, wavDuration);
+	vector<double> GuitarSound;
+	for (int i = 0; i < GuitarSound1.size(); i++) {
+		GuitarSound.push_back(GuitarSound1[i] + GuitarSound2[i]);
 	}
-	makeWav(2, seconds, "prueba", GuitarSound,volume);
+	makeWav(2, wavDuration, "prueba", GuitarSound,volume);
 	return 0;
 	}
 
