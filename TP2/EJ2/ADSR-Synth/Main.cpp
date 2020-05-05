7656609cfdc08da0301f3ea4aa3c9396fc071ba5
Alan <31071359+amechu@users.noreply.github.com>
2020-05-05 14:53:13 -0300

    asdf

--- TP2/EJ2/ADSR-Synth/Main.cpp
+++ TP2/EJ2/ADSR-Synth/Main.cpp
@@ -13,11 +13,36 @@
 
 int main(void) {
 
-	std::vector<double> harmonic_components{1, 0.668, 0.5623, 0.4466, 0.2386, 0.266, 0.1333, 0.0841, 0.0595, 6.3e-3, 5.7e-3, 3.1e-3, 2.3e-3, 2.8e-3, 1.9e-3};
-	std::vector<double> x;
-	Organ my_organ(harmonic_components, 0.05, 0, 0.75, 0.2);
+	//std::vector<double> harmonic_components{1, 0.668, 0.5623, 0.4466, 0.2386, 0.266, 0.1333, 0.0841, 0.0595, 6.3e-3, 5.7e-3, 3.1e-3, 2.3e-3, 2.8e-3, 1.9e-3};
+	std::vector<double> harmonic_components{ 
+		0.7, 0.25, 0, 0.06, 0, 0, 0, 0.02, 0.032, 0.0007
+	};
 
-	x = my_organ.generateNote(5, 220, 10); //ay dios suena horrible
+	std::vector<double> undertone_components{
+		0.5, 0.3, 0.075
+	};
+
+	std::vector<double> x1;
+	std::vector<double> x2;
+	std::vector<double> x3;
+	std::vector<double> x;
+	Organ my_organ(harmonic_components, undertone_components, 0.05, 0, 0.75, 0.2);
+
+	x1 = my_organ.generateNote(0.5, 110, 10); //ay dios suena horrible
+	x2 = my_organ.generateNote(0.5, 147, 10);
+	x3 = my_organ.generateNote(0.5, 110, 10);
+
+	for (int i = 0; i < x1.size(); i++) {
+		x.push_back(x1[i]);
+	}
+
+	for (int i = 0; i < x2.size(); i++) {
+		x.push_back(x2[i]);
+	}
+
+	for (int i = 0; i < x3.size(); i++) {
+		x.push_back(x3[i]);
+	}
 
 	makeWav(2, 5, "sintest", x, 10000);
 
--- TP2/EJ2/ADSR-Synth/Organ.h
+++ TP2/EJ2/ADSR-Synth/Organ.h
@@ -9,12 +9,26 @@
 class Organ
 {
 public:
-	Organ(std::vector<double>harmonic_content, double A, double D, double S, double R);
+	Organ(std::vector<double>harmonic_content, std::vector<double>undertone_content, double A, double D, double S, double R);
 	std::vector<double> generateNote(double duration, double pitch, double volume);
 	~Organ();
 
 private:
-	std::vector<double>harmonic_content; //Vector con los armónicos del órgano normalizados con el primer armónico con valor 1.
+	
+	//Componentes armónicas del organo
+	double sub;						//Levanta la fundamental, y los armónicos crecientes de a dos
+	double fifth;					//Levanta a partir del tercer armónico crecientes de a seis
+	double primary;					//Levanta una octava más arriba de la fundamental con armónicos crecientes de a cuatro
+	double eighth;					//Levanta otra octava más arriba con armónicos crecientes de a ocho
+	double twelveth;				//Levanta otra octava más arriba con armónicos crecientes de a doce
+	double fifteenth;				//Levanta otra octava más arriba con armónicos crecientes de a quince
+	double seventeenth;				//Levanta otra octava más arriba con armónicos crecientes de a diecisiete
+	double nineteenth;				//Levanta otra octava más arriba con armónicos crecientes de a diecinueve
+	double below;					//Levanta una octava más abajo de la fundamental
+	double perfectThirdComponent;	//Levanta una tercera perfecta arriba de la fundamental junto a sus armónicos
+	double perfectFourthComponent;  //Levanta una cuarta perfecta arriba de la fundamental junto a sus armónicos
+	double perfectFifthComponent;	//Levanta una quinta perfecta arriba de la fundamental junto a sus armónicos
+	
 	double A; //Attack
 	double D; //Delay
 	double R; //Recovery
