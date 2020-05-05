8f76266af362d9e49b84ea9314411a4240a3b5a4
Alan <31071359+amechu@users.noreply.github.com>
2020-05-05 18:05:53 -0300

    asf

--- TP2/EJ2/ADSR-Synth/Organ.h
+++ TP2/EJ2/ADSR-Synth/Organ.h
@@ -9,13 +9,13 @@
 class Organ
 {
 public:
-	Organ(std::vector<double>harmonic_content, std::vector<double>undertone_content, double A, double D, double S, double R);
+	Organ(double sub, double fifth, double primary, double eight, double twelveth, double fifteenth, double seventeenth, double nineteenth, double below, double third, double fourth, double perfect_fifth, int sub_harmonic_count, int mix_harmonic_count, double A = 0.05, double D = 0, double S = 0.75, double R = 0.2);
 	std::vector<double> generateNote(double duration, double pitch, double volume);
 	~Organ();
 
 private:
 	
-	//Componentes armónicas del organo
+	//Constantes para el cálculo de armónicos del organo
 	double sub;						//Levanta la fundamental, y los armónicos crecientes de a dos
 	double fifth;					//Levanta a partir del tercer armónico crecientes de a seis
 	double primary;					//Levanta una octava más arriba de la fundamental con armónicos crecientes de a cuatro
@@ -25,13 +25,25 @@ private:
 	double seventeenth;				//Levanta otra octava más arriba con armónicos crecientes de a diecisiete
 	double nineteenth;				//Levanta otra octava más arriba con armónicos crecientes de a diecinueve
 	double below;					//Levanta una octava más abajo de la fundamental
-	double perfectThirdComponent;	//Levanta una tercera perfecta arriba de la fundamental junto a sus armónicos
-	double perfectFourthComponent;  //Levanta una cuarta perfecta arriba de la fundamental junto a sus armónicos
-	double perfectFifthComponent;	//Levanta una quinta perfecta arriba de la fundamental junto a sus armónicos
+	double majorThird;	//Levanta una tercera perfecta arriba de la fundamental junto a sus armónicos
+	double perfectFourth;  //Levanta una cuarta perfecta arriba de la fundamental junto a sus armónicos
+	double perfectFifth;	//Levanta una quinta perfecta arriba de la fundamental junto a sus armónicos
+
+	int sub_harmonic_count;
+	int mix_harmonic_count;
 	
-	double A; //Attack
-	double D; //Delay
-	double R; //Recovery
-	double S; //Sustain
+	//Armónicos del organo
+	std::vector<double> sub_harmonics;
+	std::vector<double> fifth_harmonics;
+	std::vector<double> primary_harmonics;
+	std::vector<double> eighth_harmonics;
+	std::vector<double> twelveth_harmonics;
+	std::vector<double> fifteenth_harmonics;
+	std::vector<double> seventeenth_harmonics;
+	std::vector<double> nineteenth_harmonics;
+	std::vector<double> below_harmonics;
+	std::vector<double> major_third_harmonics;
+	std::vector<double> perfect_fourth_harmonics;
+	std::vector<double> perfect_fifth_harmonics;
 
 };
