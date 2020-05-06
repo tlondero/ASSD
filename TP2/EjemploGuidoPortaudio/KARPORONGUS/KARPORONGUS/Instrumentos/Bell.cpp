247bced810890e472ed7b1f1862f237dd41c168a
tlondero <tlondero@itba.edu.ar>
2020-05-06 13:43:37 -0300

    For whom the bell tolls

---
+++ TP2/EjemploGuidoPortaudio/KARPORONGUS/KARPORONGUS/Instrumentos/Bell.cpp
@@ -0,0 +1,39 @@
+#include "Bell.h"
+#include <cstdlib> 
+#include <algorithm>
+#include <ctime>
+#include <random>
+
+Bell::Bell() {
+	this->n = 1;
+	this->m = 2;
+	this->tau = 0.5;
+	this->io = 0.5;
+}
+
+vector <double> Bell::generateNote(double duration, double pitch, double Normvelocity) {
+	vector <double> BellSound;
+	double fc = pitch* (this->m);
+	double fm = pitch* (this->n);
+
+	double Idet;
+	double Adet;
+	
+	int normtime = (int)duration * SAMPLE_RATE;
+
+	for (int i = 0; i < (int)duration * SAMPLE_RATE; i++) {
+		Idet = (this->io) * exp(-i / (normtime * (this->tau)));
+		Adet = exp( -i / (normtime * (this->tau)) );
+		BellSound.push_back( Adet*( cos( (DOSPI * fc * i / SAMPLE_RATE) + Idet * cos(DOSPI * fm * i / SAMPLE_RATE))*Normvelocity ));
+	}
+
+	double max = *max_element(BellSound.begin(), BellSound.end());
+	for (int i = 0; i < BellSound.size(); i++) {
+		BellSound[i] = BellSound[i] / max;
+	}
+
+	return BellSound;
+}
+
+Bell::~Bell() {
+}
