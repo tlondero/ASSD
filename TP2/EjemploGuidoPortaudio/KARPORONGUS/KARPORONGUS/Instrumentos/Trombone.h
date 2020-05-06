aa083f6fb58d016e3292eebc15991285b88489c1
tlondero <tlondero@itba.edu.ar>
2020-05-06 16:32:32 -0300

    trombone

---
+++ TP2/EjemploGuidoPortaudio/KARPORONGUS/KARPORONGUS/Instrumentos/Trombone.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include "Instrument.h"
+#include "../General.h"
+
+#define DOSPI 6.283185307179586476925286766559
+#define PI DOSPI/2
+
+using namespace std;
+
+class Trombone : public Instrument
+{
+public:
+	Trombone();
+	~Trombone();
+	
+	vector <double> generateNote(double duration, double pitch, double Normvelocity);
+
+private:
+	double n;
+	double m;
+	double io;
+};
