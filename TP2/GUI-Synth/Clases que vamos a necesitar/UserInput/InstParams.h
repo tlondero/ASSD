05b821ba06916dba15216103e25cdd59ca2879fa
Guido <glambertucci@itba.edu.ar>
2020-05-08 16:41:40 -0300

    agregando clases para hacer la gui

---
+++ TP2/GUI-Synth/Clases que vamos a necesitar/UserInput/InstParams.h
@@ -0,0 +1,12 @@
+#pragma once
+class InstParams
+{
+public:
+	InstParams();
+	~InstParams();
+	double GuitarParam_rf=1;
+	double DrumParam_rf = 1;
+	double DrumProb = 0.5;
+	//aca se ponene todos los parametros que puede necesitar un instrumento
+};
+
