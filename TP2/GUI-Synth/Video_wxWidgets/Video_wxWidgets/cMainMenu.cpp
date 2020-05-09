673f758a1b9b51267dea74e63a279a0db16e1135
tlondero <tlondero@itba.edu.ar>
2020-05-09 18:41:09 -0300

    Update cMainMenu.cpp

--- TP2/GUI-Synth/Video_wxWidgets/Video_wxWidgets/cMainMenu.cpp
+++ TP2/GUI-Synth/Video_wxWidgets/Video_wxWidgets/cMainMenu.cpp
@@ -393,23 +393,25 @@ void cMainMenu::AddMidiToProgram(wxCommandEvent& evt) {
 
 void cMainMenu::CreateWav(wxCommandEvent& evt) {
 	if (!(lb_tracks->IsEmpty())) {
-		wxFileDialog saveFileDialog(this, _("Save WAV file"), "", "", "WAV files (*.wav)|*.wav", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
-		if (saveFileDialog.ShowModal() == wxID_CANCEL) {			//Esto está por si se cierra el explorador sin elegir archivos
-			return;
-		}
+		myWC.compileWav(myCC.sytnsynthesisProject(this->midiTranslated, this->ui), this->midi.getTotalDuration() + 1, pathSelected, 1000);
+		myWC.makeWav();
+		//wxFileDialog saveFileDialog(this, _("Save WAV file"), "", "", "WAV files (*.wav)|*.wav", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
+		//if (saveFileDialog.ShowModal() == wxID_CANCEL) {			//Esto está por si se cierra el explorador sin elegir archivos
+		//	return;
+		//}
 
-		string pathSelected = saveFileDialog.GetPath();				//Path completo
-		wxFileOutputStream output_stream(saveFileDialog.GetPath());
+		//string pathSelected = saveFileDialog.GetPath();				//Path completo
+		//wxFileOutputStream output_stream(saveFileDialog.GetPath());
 
-		if (!output_stream.IsOk()) {
-			wxLogError("Cannot save file '%s'.", pathSelected);
-		}
-		else {
-			pathSelected = pathSelected.erase(pathSelected.find_first_of(".wav"), pathSelected.size());
+		//if (!output_stream.IsOk()) {
+		//	wxLogError("Cannot save file '%s'.", pathSelected);
+		//}
+		//else {
+		//	pathSelected = pathSelected.erase(pathSelected.find_first_of(".wav"), pathSelected.size());
 
-			myWC.compileWav(myCC.sytnsynthesisProject(this->midiTranslated, this->ui), this->midi.getTotalDuration() + 1, pathSelected, 1000);
-			myWC.makeWav();
-		}
+		//	myWC.compileWav(myCC.sytnsynthesisProject(this->midiTranslated, this->ui), this->midi.getTotalDuration() + 1, pathSelected, 1000);
+		//	myWC.makeWav();
+		//}
 	}
 	evt.Skip();
 }
