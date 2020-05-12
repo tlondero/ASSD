f09dcfd87c1da8550a7f611aae57026738cebe12
Alan <31071359+amechu@users.noreply.github.com>
2020-05-12 10:34:11 -0300

    asfasf

--- TP2/GUI-Synth/Video_wxWidgets/Video_wxWidgets/Efectos/Reverberator.cpp
+++ TP2/GUI-Synth/Video_wxWidgets/Video_wxWidgets/Efectos/Reverberator.cpp
@@ -10,14 +10,14 @@ Reverberator::~Reverberator()
 {
 }
 
-std::vector<double> Reverberator::applyEffect(std::vector<double> input, double T, int mix)
+std::vector<double> Reverberator::applyEffect(std::vector<double> input, double T, double mix)
 {
 
 //T = 3tn/(-log|gn|) para comb paralelo del paper de Schröder, asimismo resto de los valores fueron sacados del paper.
 
 std::vector<double> M_series = { 1051, 337 };
 std::vector<double> g_series = { -0.7, -0.7 };
-std::vector<double> M_parallel = { 0.108820861, 0.113356009, 0.1224263038, 0.1315419501 , 0.1408843537 };
+std::vector<double> M_parallel = { 4799, 4999, 5399, 5801, 6203};
 std::vector<double> g_parallel;
 
 for (int i = 0; i < T * SAMPLE_RATE; i++) {
@@ -28,13 +28,13 @@ std::vector<double> wavVector2(input.size(), 0);
 std::vector<double> temp(input.size(), 0);
 
 for (int i = 0; i < M_parallel.size(); i++) {
-	g_parallel.push_back(-1*pow(10, (-3*M_parallel[i])/(T)));
+	g_parallel.push_back(-1*pow(10, (-3*M_parallel[i])/(T*SAMPLE_RATE)));
 }
 
 for (int j = 0; j < M_parallel.size(); j++) {
 	for (int i = 0; i < input.size(); i++) {
-		if (i - M_parallel[j] > 0) {
-			temp[i] = input[i] + g_parallel[j] * input[i - M_parallel[j]];
+		if (i - (M_parallel[j]) > 0) {
+			temp[i] = input[i] + g_parallel[j] * input[i - (M_parallel[j])];
 		}
 		else {
 			temp[i] = input[i];
@@ -61,5 +61,6 @@ for (int i = 0; i < input.size(); i++) {
 
 
 	return input;
+
 }
 
