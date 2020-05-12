6a5d9c81570dac949630e1889ced6c4ea81757fc
Guido <glambertucci@itba.edu.ar>
2020-05-11 21:15:50 -0300

    merge

--- TP2/GUI-Synth/Video_wxWidgets/Video_wxWidgets/Controllers/ControllerOfControllers.cpp
+++ TP2/GUI-Synth/Video_wxWidgets/Video_wxWidgets/Controllers/ControllerOfControllers.cpp
@@ -1,17 +1,22 @@
 #include "ControllerOfControllers.h"
 
-ControllerOfControllers::ControllerOfControllers(){
+ControllerOfControllers::ControllerOfControllers() {
 }
 
 vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> projectTracks, UserInput userData) {
 	vector<SynthTrack> mySynthTrackVector;
 	for (unsigned int track = 0; track < userData.pairTrackInst.size(); track++) {
 		SynthTrack tempTrack;
+		if (userData.finalEfect != "") {
+			tempTrack.effect2Apply = userData.finalEfect;
+		}
 		if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[0]) {
 			//AGREGAR EL BOOL DE PREVIEW
 			this->AGController.setParam(userData.pairTrackInst[track].params.GuitarParam_rf);
 			tempTrack = this->AGController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (!userData.pairTrackInst[track].effect2Apply.empty())
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -20,7 +25,8 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[9]) {
 			this->EGController.setParam(userData.pairTrackInst[track].params.GuitarParam_rf);
 			tempTrack = this->EGController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -28,7 +34,8 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		}
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[8]) {
 			tempTrack = this->BController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -37,7 +44,8 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[7]) {
 			this->DController.setParam(userData.pairTrackInst[track].params.DrumParam_rf, userData.pairTrackInst[track].params.DrumProb);
 			tempTrack = this->DController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -45,15 +53,17 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		}
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[5]) {
 			tempTrack = this->TController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
 			mySynthTrackVector.push_back(tempTrack);
 		}
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[3]) {
-			tempTrack = this->bellController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber],userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			tempTrack = this->bellController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -61,7 +71,8 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		}
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[4]) {
 			tempTrack = this->CController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -70,7 +81,8 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[1] || userData.pairTrackInst[track].TrackInstrument == InstrumentList[2]) {
 			projectTracks[userData.pairTrackInst[track].TrackNumber].userInstrumentChoice = userData.pairTrackInst[track].TrackInstrument;
 			tempTrack = this->OController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -78,7 +90,8 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 		}
 		else if (userData.pairTrackInst[track].TrackInstrument == InstrumentList[6]) {
 			tempTrack = this->TrumpController.sytnsynthesisTrack(projectTracks[userData.pairTrackInst[track].TrackNumber], userData.pairTrackInst[track].InstrumentPreview);
-			tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
+			if (userData.pairTrackInst[track].effect2Apply != "")
+				tempTrack.effect2Apply = userData.pairTrackInst[track].effect2Apply;
 			tempTrack.T = userData.pairTrackInst[track].T;
 			tempTrack.M = userData.pairTrackInst[track].M;
 			tempTrack.D = userData.pairTrackInst[track].D;
@@ -93,5 +106,5 @@ vector<SynthTrack> ControllerOfControllers::sytnsynthesisProject(vector<Tracks> 
 }
 
 
-ControllerOfControllers::~ControllerOfControllers(){
+ControllerOfControllers::~ControllerOfControllers() {
 }
