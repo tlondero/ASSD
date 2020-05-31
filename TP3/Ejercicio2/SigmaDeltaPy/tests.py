8addf2c65fa19b426b840e875d16d7dbd0657dd4
MT2321 <mrodriguez@itba.edu.ar>
2020-05-31 02:51:32 -0300

    No conviene saturar al DA

--- TP3/Ejercicio2/SigmaDeltaPy/tests.py
+++ TP3/Ejercicio2/SigmaDeltaPy/tests.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 
 
 def test1():
+    # PRUEBA A FULL SCALE
     sd = SigmaDelta()
     f = 5e+2  # Frecuencia fundamental de la senoidal de prueba
     fs = 25*f  # Frecuencia de sampleo de mi señal, con esto generamos las muestras
     # El espaciado entre las muestras es función de la frecuencia de muestreo
     time = np.arange(0, 2e-3, 1/fs)
-    sine = np.sin(2*np.pi*f*time)
+    sine = 0.8*np.sin(2*np.pi*f*time)
     # Por el momento sd.modulator devuelve la señal resampleada (con más samples) para poder plotear
     ry, rt, y = sd.modulator(time, sine, 64)
     plt.plot(rt, ry)
@@ -19,6 +20,7 @@ def test1():
 
 
 def test2():
+    # PRUEBA A FULL SCALE
     sd = SigmaDelta()
     f = 5e+2  # Frecuencia fundamental de la senoidal de prueba
     fs = 25*f  # Frecuencia de sampleo de mi señal, con esto generamos las muestras
@@ -46,13 +48,31 @@ def test3():
     # Por el momento sd.modulator devuelve la señal resampleada (con más samples) para poder plotear
     ry, rt, y = sd.modulator(time, sine, 64)
 
-    plt.title("Test3 Amplitud 0.5")
+    plt.title("Promedios Test3 Amplitud 0.5")
     # Aca ploteamos el historial de promedios
     plt.plot(rt, ry)
     plt.plot(rt[:-1], sd.historyChannel)
     plt.show()
 
 
+def test4():
+    # PRUEBA mitad de rango
+    sd = SigmaDelta()
+    f = 5e+2  # Frecuencia fundamental de la senoidal de prueba
+    fs = 25*f  # Frecuencia de sampleo de mi señal, con esto generamos las muestras
+    # El espaciado entre las muestras es función de la frecuencia de muestreo
+    time = np.arange(0, 2e-3, 1/fs)
+    sine = 0.5*np.sin(2*np.pi*f*time)
+    # Por el momento sd.modulator devuelve la señal resampleada (con más samples) para poder plotear
+    ry, rt, y = sd.modulator(time, sine, 64)
+    plt.title("Bitstream con Amplitud 0.5")
+    plt.plot(rt, ry)
+    # Le quitamos los primeros 2 valores porque son artificiales
+    plt.plot(rt, y[2:])
+    plt.show()
+
+
 test1()
 test2()
 test3()
+test4()
